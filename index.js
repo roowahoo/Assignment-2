642180bc14ae5b24336554fe8510fb8f883ed12e
roowahoo <tohyinru95@gmail.com>
2021-02-28 06:56:59 +0000

    added get post endpoint

--- index.js
+++ index.js
@@ -13,5 +13,41 @@ app.use(cors())
 async function main(){
     let db=await MongoUtil.connect(mongoUrl, 'profiles')
 
+    app.get('/profiles',async (req,res)=>{
+        let results=await db.collection('profiles').find({}).toArray()
+        res.status(200)
+        res.send(results)
+    })
+
+    app.post('/profiles', async (req,res)=>{
+        let name=req.body.name;
+        let age=req.body.age;
+        let gender=req.body.gender;
+        let interests=req.body.interests;
+        let contact=req.body.contact
+
+        try{
+            let result=db.collection('profiles').insertOne({
+                name:name,
+                age:age,
+                gender:gender,
+                interests:interests,
+                contact:contact
+            })
+            res.status(200)
+            res.send(result)
+        }catch(e){
+            res.status(500)
+            res.send({
+                error:'Internal server error'
+            })
+            console.log(e)
+        }
+    })
+
 }
 main()
+
+app.listen(3001,()=>{
+    console.log('Server has started')
+})
