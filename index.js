19fc0bb1c72f9c40acdb32bce48b565494150481
roowahoo <tohyinru95@gmail.com>
2021-02-28 10:42:05 +0000

    added more collections

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 async function main(){
-    let db=await MongoUtil.connect(mongoUrl, 'profiles')
+    let db=await MongoUtil.connect(mongoUrl, 'dating_app')
 
     app.get('/profiles',async (req,res)=>{
         let results=await db.collection('profiles').find({}).toArray()
@@ -19,20 +19,38 @@ async function main(){
         res.send(results)
     })
 
+    app.get('/conversations',async (req,res)=>{
+        let results=await db.collection('connections').find({}).toArray()
+        res.status(200)
+        res.send(results)
+    })
+
+    app.get('/matches',async (req,res)=>{
+        let results=await db.collection('stars').find({}).toArray()
+        res.status(200)
+        res.send(results)
+    })
+
     app.post('/profiles', async (req,res)=>{
+        let account_id=req.body.account_id
         let name=req.body.name;
         let age=req.body.age;
         let gender=req.body.gender;
         let interests=req.body.interests;
-        let contact=req.body.contact
+        let contact=req.body.contact;
+        let likes=req.body.likes;
+        let introduction=req.body.introduction
 
         try{
             let result=db.collection('profiles').insertOne({
+                account_id:account_id,
                 name:name,
                 age:age,
                 gender:gender,
                 interests:interests,
-                contact:contact
+                contact:contact,
+                likes:likes,
+                introduction:introduction
             })
             res.status(200)
             res.send(result)
@@ -44,6 +62,49 @@ async function main(){
             console.log(e)
         }
     })
+
+    app.post('/conversations', async (req,res)=>{
+        let conversation_id=req.body.conversation_id
+        let users_ids=req.body.users_id;
+        
+
+        try{
+            let result=db.collection('conversations').insertOne({
+                conversation_id:conversation_id,
+                users_id:users_ids
+            })
+            res.status(200)
+            res.send(result)
+        }catch(e){
+            res.status(500)
+            res.send({
+                error:'Internal server error'
+            })
+            console.log(e)
+        }
+    })
+
+    app.post('/matches', async (req,res)=>{
+        let matched_id=req.body.matched_id
+        let users_ids=req.body.users_id;
+        
+
+        try{
+            let result=db.collection('matches').insertOne({
+                matched_id:matched_id,
+                users_id:users_ids
+            })
+            res.status(200)
+            res.send(result)
+        }catch(e){
+            res.status(500)
+            res.send({
+                error:'Internal server error'
+            })
+            console.log(e)
+        }
+    })
+
 
 }
 main()
